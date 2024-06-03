2e973b64a773c4545d467fc4a61f12867eacbf5f
tkfkadlsi <yeseonggim35@gmail.com>
2024-06-03 12:56:54 +0900

    asdf

---
+++ CPPTeamProject/CPPTeamProject/Console.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::wcout;
+BOOL Gotoxy(int x, int y);
+COORD CursorPos();
+void SetCursorVis(bool vis, DWORD size);
+void SetColor(int textColor, int bgColor);
+int GetColor();
+COORD GetConsoleResolution();
+void SetFontSize(UINT weight, UINT fontx, UINT fonty);
+
+enum class COLOR
+{
+	BLACK, BLUE, GREEN, SKYBLUE, RED,
+	VOILET, YELLOW, LIGHT_GRAY, GRAY, LIGHT_BLUE,
+	LIGHT_GREEN, MINT, LIGHT_RED, LIGHT_VIOLET,
+	LIGHT_YELLOW, WHITE, END
+};
