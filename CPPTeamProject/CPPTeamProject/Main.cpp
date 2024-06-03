b461248173d1801dad33d50636554639c53d7007
qls051 <qls050100@naver.com>
2024-06-03 18:13:59 +0900

    TitleScene

--- CPPTeamProject/CPPTeamProject/Main.cpp
+++ CPPTeamProject/CPPTeamProject/Main.cpp
@@ -1,1 +1,8 @@
-﻿	1
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
--- CPPTeamProject/CPPTeamProject/TitleScene.cpp
+++ CPPTeamProject/CPPTeamProject/TitleScene.cpp
@@ -1,9 +1,11 @@
-﻿#include<iostream>
+﻿#include <iostream>
 #include<conio.h>
 #include<io.h>
 #include<fcntl.h>
+#include<windows.h>
 
-#include"TitleScene.h"
+#include "TitleScene.h"
+#include "Console.h"
 
 using namespace std;
 
@@ -21,11 +23,13 @@ void TitleRender()
 
 	int curoremode = _setmode(_fileno(stdout), beforemode);
 }
+
+
 void InfoRender()
 {
 	system("cls");
 	cout << "조작법" << endl;
-	cout << "스토리" << endl;
+	cout << "게임 설명" << endl;
 	cout << "제작" << endl;
 	Sleep(100);
 	while (true)
@@ -36,6 +40,71 @@ void InfoRender()
 			break;
 		}
 	}
+}
+
+MENU MenuRender()
+{
+	COORD Resolution = GetConsoleResolution(); // 해상도 출력
+	int x = Resolution.X / 4.5;
+	int y = Resolution.Y / 4;
+	int originy = y;
+
+	Gotoxy(x, y);
+	cout << "게임 시작";
+	Gotoxy(x, y + 1);
+	cout << "게임 정보";
+	Gotoxy(x, y + 2);
+	cout << "게임 종료";
+
+	while (true)
+	{
+		// 키 입력이 된 것을 받아옴
+		KEY ekey = KeyController();
+
+		switch (ekey)
+		{
+			// 화살표 출력
+			// 화살표 지우기
+		case KEY::UP:
+		{
+			if (y > originy)
+			{
+				Gotoxy(x - 2, y);
+				cout << " ";
+				Gotoxy(x - 2, --y);
+				cout << ">";
+				Sleep(100);
+			}
+		}
+		break;
+		case KEY::DOWN:
+		{
+			if (y < originy + 2)
+			{
+				Gotoxy(x - 2, y);
+				cout << " ";
+				Gotoxy(x - 2, ++y);
+				cout << ">";
+				Sleep(100);
+			}
+		}
+		break;
+		case KEY::SPACE:
+		{
+			if (originy == y)
+				return MENU::START;
+
+			else if (originy + 1 == y)
+				return MENU::INFO;
+
+			else if (originy + 2 == y)
+				return MENU::QUIT;
+		}
+		break;
+		}
+	}
+
+	return MENU::INFO;
 }
 
 KEY KeyController()
@@ -56,3 +125,25 @@ KEY KeyController()
 	return KEY::FALE;
 }
 
+bool TitleScene()
+{
+	TitleRender();
+	while (true)
+	{
+		MENU eMenu = MenuRender();
+
+		switch (eMenu)
+		{
+		case MENU::START:
+			//EnterAnimation();
+			return true;
+			break;
+		case MENU::INFO:
+			InfoRender();
+			break;
+		case MENU::QUIT:
+			return false;
+			break;
+		}
+	}
+}
